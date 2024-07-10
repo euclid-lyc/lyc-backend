66316cd7bd17880d2a7688317a22b3cacdbbecb2
dvlp-sy <sy8389b@gmail.com>
2024-07-11 02:48:45 +0900

    [add] Add PushSet Domain Class

---
+++ lyc-spring/src/main/java/euclid/lyc_spring/domain/PushSet.java
@@ -0,0 +1,36 @@
+package euclid.lyc_spring.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class PushSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(columnDefinition = "BIT DEFAULT 1")
+    private Boolean dm;
+
+    @Column(columnDefinition = "BIT DEFAULT 1")
+    private Boolean feed;
+
+    @Column(columnDefinition = "BIT DEFAULT 1")
+    private Boolean schedule;
+
+    @Column(columnDefinition = "BIT DEFAULT 1")
+    private Boolean likeMark;
+
+    @Column(columnDefinition = "BIT DEFAULT 1")
+    private Boolean event;
+
+    @Column(columnDefinition = "BIT DEFAULT 1")
+    private Boolean ad;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
