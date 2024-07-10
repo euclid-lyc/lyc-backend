3a4403c5bf25200a227a6663d1bedb17d11bb5c4
dvlp-sy <sy8389b@gmail.com>
2024-07-10 18:08:00 +0900

    [chore] Rename NotificationType

--- lyc-spring/src/main/java/euclid/lyc_spring/domain/enums/CommissionStatus.java
+++ lyc-spring/src/main/java/euclid/lyc_spring/domain/enums/CommissionStatus.java
@@ -1,6 +1,6 @@
 package euclid.lyc_spring.domain.enums;
 
-public enum NotificationType {
+public enum CommissionStatus {
     REQUIRED, // 의뢰 요청
     APPROVED, // 의뢰 승인
     WAIT_FOR_TERMINATION, // 의뢰 종료 대기
