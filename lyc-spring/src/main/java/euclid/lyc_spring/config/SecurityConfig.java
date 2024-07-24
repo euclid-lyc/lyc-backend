49cf32d33933db8d0284246ec10c95abba0dd519
dvlp-sy <sy8389b@gmail.com>
2024-07-24 09:44:51 +0900

    [modify] Modify SecurityConfig

--- lyc-spring/src/main/java/euclid/lyc_spring/config/SecurityConfig.java
+++ lyc-spring/src/main/java/euclid/lyc_spring/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package euclid.lyc_spring.config;
 
+import euclid.lyc_spring.auth.JwtAuthenticationFilter;
+import euclid.lyc_spring.auth.JwtProvider;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,9 +13,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -23,20 +30,33 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HttpSecurity httpSecurity) throws Exception {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
-        return httpSecurity
-                .httpBasic(AbstractHttpConfigurer::disable) // disable basic auth
-                .csrf(AbstractHttpConfigurer::disable) // disable csrf
-                .formLogin(AbstractHttpConfigurer::disable) // disable form login
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)                   // disable basic auth
+                .csrf(AbstractHttpConfigurer::disable)                           // disable csrf
+                .formLogin(AbstractHttpConfigurer::disable)                      // disable form login
                 .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // use stateless session
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/register").permitAll() // 회원가입에만 모든 요청 허가
-                        .anyRequest().hasAuthority("ROLE_MEMBER")) // 나머지 요청은 멤버 권한이 있어야 허가
-                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class) // JWT 인증 필터 처리 -> 패스워드 인증 필터 처리
-                .requiresChannel(requiresChannel -> requiresChannel // 모든 요청을 https로 강제
-                        .anyRequest().requiresSecure())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/api/register", "/api/sign-in").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().denyAll()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
