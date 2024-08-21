1e82ef76a0b07d76c309d781361948c3d7d44db8
Chaewon Lee <let022@kangwon.ac.kr>
2024-08-21 21:13:26 +0900

    .

--- lyc-spring/src/main/java/euclid/lyc_spring/dto/request/InfoRequestDTO.java
+++ lyc-spring/src/main/java/euclid/lyc_spring/dto/request/InfoRequestDTO.java
@@ -3,6 +3,7 @@ package euclid.lyc_spring.dto.request;
 import euclid.lyc_spring.domain.enums.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -34,6 +35,12 @@ public class InfoRequestDTO {
 
         private List<Style> preferredStyleList;
         private List<Style> nonPreferredStyleList;
+
+        // test
+        public InfoStyleListDTO(List<Style> styles) {
+            this.preferredStyleList = styles;
+            this.nonPreferredStyleList = styles;
+        }
     }
 
     @Getter
@@ -43,6 +50,12 @@ public class InfoRequestDTO {
 
         private List<Fit> preferredFitList;
         private List<Fit> nonPreferredFitList;
+
+        // test
+        public InfoFitListDTO(List<Fit> Fits) {
+            this.preferredFitList = Fits;
+            this.nonPreferredFitList = Fits;
+        }
     }
 
     @Getter
@@ -52,6 +65,12 @@ public class InfoRequestDTO {
 
         private List<Material> preferredMaterialList;
         private List<Material> nonPreferredMaterialList;
+
+        // test
+        public <E> InfoMaterialListDTO(List<Material> es) {
+            this.preferredMaterialList = es;
+            this.nonPreferredMaterialList = es;
+        }
     }
 
     @Getter
@@ -61,6 +80,10 @@ public class InfoRequestDTO {
 
         private List<BodyType> goodBodyTypeList;
         private List<BodyType> badBodyTypeList;
+
+        //test
+        public <E> InfoBodyTypeListDTO(List<BodyType> es) {
+        }
     }
 
 }
