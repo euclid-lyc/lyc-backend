79e1761cf0a20fc5aca65ced18dc8d07d8558662
dvlp-sy <sy8389b@gmail.com>
2024-08-28 17:19:27 +0900

    [feat] Implement uploadImage

---
+++ lyc-spring/src/main/java/euclid/lyc_spring/service/s3/S3ImageService.java
@@ -0,0 +1,13 @@
+package euclid.lyc_spring.service.s3;
+
+import euclid.lyc_spring.dto.s3.S3DTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface S3ImageService {
+
+    S3DTO.ImageListDTO uploadImagesToBucket(List<MultipartFile> images);
+    String upload(MultipartFile image);
+
+}
