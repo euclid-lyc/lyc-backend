afca6f18cd1f7459655cf0a7c9b27c1b8093618a
Chaewon Lee <let022@kangwon.ac.kr>
2024-09-05 13:50:22 +0900

    .

---
+++ lyc-spring/src/main/java/euclid/lyc_spring/service/search/SearchServiceImpl.java
@@ -0,0 +1,11 @@
+package euclid.lyc_spring.service.search;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SearchServiceImpl implements SearchService {
+}
