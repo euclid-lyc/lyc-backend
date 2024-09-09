04a8fc1c3f0d048ead2ac369158ce775ab49bd17
dvlp-sy <sy8389b@gmail.com>
2024-09-09 16:39:55 +0900

    [add] Add Repository Methods

--- lyc-spring/src/main/java/euclid/lyc_spring/repository/ScheduleRepository.java
+++ lyc-spring/src/main/java/euclid/lyc_spring/repository/ScheduleRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    void deleteAllByChatId(Long id);
 }
