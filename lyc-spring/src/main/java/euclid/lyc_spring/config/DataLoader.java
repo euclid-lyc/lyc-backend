71b19966d147380073d5143441d943fc24f57b8c
Chaewon Lee <let022@kangwon.ac.kr>
2024-09-13 18:45:48 +0900

    .

---
+++ lyc-spring/src/main/java/euclid/lyc_spring/config/DataLoader.java
@@ -0,0 +1,42 @@
+package euclid.lyc_spring.config;
+
+import euclid.lyc_spring.domain.chat.Message;
+import euclid.lyc_spring.domain.mapping.MemberChat;
+import euclid.lyc_spring.repository.MemberChatRepository;
+import euclid.lyc_spring.repository.MessageRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader {
+
+//    @Bean
+//    public CommandLineRunner loadData(MessageRepository messageRepository, MemberChatRepository memberChatRepository) {
+//        return args -> {
+//            // MemberChat 객체를 먼저 생성해야 합니다.
+//            // 이를 위한 더미 데이터 생성 예제입니다.
+//            MemberChat memberChat1 = memberChatRepository.findById(11L).orElseThrow(); // 적절한 데이터를 세팅해 주세요.
+//            MemberChat memberChat2 = memberChatRepository.findById(12L).orElseThrow();
+//
+//            // 더미 데이터 생성
+//            Message message1 = Message.builder()
+//                    .content("안녕하세요")
+//                    .isText(true)
+//                    .isChecked(false)
+//                    .memberChat(memberChat1)
+//                    .build();
+//
+//            Message message2 = Message.builder()
+//                    .content("안녕못해요")
+//                    .isText(true)
+//                    .isChecked(true)
+//                    .memberChat(memberChat2)
+//                    .build();
+//
+//            // 저장
+//            messageRepository.save(message1);
+//            messageRepository.save(message2);
+//        };
+//    }
+}
