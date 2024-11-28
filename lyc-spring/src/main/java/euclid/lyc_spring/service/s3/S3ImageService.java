38d45db409c9eb3768614b3d09410b3a4494875f
dvlp-sy <sy8389b@gmail.com>
2024-11-28 11:15:31 +0900

    [modify] Update S3ImageService

--- lyc-spring/src/main/java/euclid/lyc_spring/service/s3/S3ImageService.java
+++ lyc-spring/src/main/java/euclid/lyc_spring/service/s3/S3ImageService.java
@@ -3,11 +3,13 @@ package euclid.lyc_spring.service.s3;
 import euclid.lyc_spring.dto.s3.S3DTO;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
 import java.util.List;
 
 public interface S3ImageService {
 
     String upload(MultipartFile image);
+    String upload(InputStream inputStream, String extension, String filename);
     void deleteImageFromS3(String imageAddress);
 
 }
